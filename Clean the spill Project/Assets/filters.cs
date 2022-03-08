2aa7168ce0e38417b25ed6598a2936a950d676f1
mergimgashi <56248317+mergimgashi@users.noreply.github.com>
2022-03-08 14:27:12 +0100

    filters

---
+++ Clean the spill Project/Assets/filters.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class filters : MonoBehaviour
+{
+    // Use this for initialization
+ void Start () {
+     
+ }
+ 
+ // Update is called once per frame
+ void Update () {
+     if (Input.GetKeyDown(KeyCode.Q))
+     {
+         gameObject.GetComponent<Renderer>().material.color = new Color(1.0f, 0.0f, 0.0f, 0.4f);
+     }
+     if (Input.GetKeyDown(KeyCode.W))
+     {
+         gameObject.GetComponent<Renderer>().material.color = new Color(0.0f, 1.0f, 0.0f, 0.4f);
+     }
+     if (Input.GetKeyDown(KeyCode.E))
+     {
+         gameObject.GetComponent<Renderer>().material.color = new Color(0.0f, 0.0f, 1.0f, 0.4f);;
+     }
+     if (Input.GetKeyDown(KeyCode.R))
+     {
+         gameObject.GetComponent<Renderer>().material.color = new Color(0.0f, 0.0f, 0.0f, 0.0f);
+     }
+ }
+}
