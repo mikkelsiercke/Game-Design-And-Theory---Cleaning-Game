0ea82a44e1e5daa23262caa44704f5c1610e64c1
Slobodziuk <47206643+Slobodziuk@users.noreply.github.com>
2022-03-29 15:45:24 +0200

    Forsøgt at tilføje Color Grading med color filter
    Forsøgt at tilføje Color Grading med color filter.

--- Clean the spill Project/Assets/Scripts/filter_Script.cs
+++ Clean the spill Project/Assets/Scripts/filter_Script.cs
@@ -5,20 +5,41 @@ using UnityEngine.Rendering.PostProcessing;
 using System.Collections;
 
 public class filter_Script : MonoBehaviour {
-
 public PostProcessVolume volume;
+
+
 private Vignette _Vignette;
+//private ColorGrading colorFilter;
+
+public bool filter;
+public float saturation = 5f;
 
     void Start(){
+    //Vignette
     volume.profile.TryGetSettings(out _Vignette);
+
+   //volume.profile.TryGetSettings(out colorFilter);
+
     _Vignette.intensity.value = 0;
+
+    //Filter aktiveret eller ej
+    filter = false;
 
     }
 
     void Update(){
-        if(Input.GetKeyDown("space")){
+        //If statement der aktiverer filtret
+        if(Input.GetKeyDown("space") && filter == false){
+            //Sætter Vignetten til 1 samt sætter boolean der kontrollerer filt er til true
            _Vignette.intensity.value = 1;
-        } 
+            filter = true;
+            //colorFilter.enable.value = true;
+        }else if (Input.GetKeyDown("space") && filter == true){
+            //Fjerne vignette og sætte boolean til false
+           _Vignette.intensity.value = 0;
+            filter = false;
+            //colorFilter.enable.value = false;
+        }
     }
 }
 
