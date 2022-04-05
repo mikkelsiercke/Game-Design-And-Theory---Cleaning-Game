572dc74b280784013fb632b05c8e8194641036ed
Silleth <50498935+Silleth@users.noreply.github.com>
2022-04-05 17:09:37 +0200

    Revert "Added two assets"
    This reverts commit 1d4f078d2e9950336b6cd99b8b5114d5f3f0893e, reversing
    changes made to 9c4f771daffe6d1cd52d8b66e446ddaeaeb821ff.

--- Clean the spill Project/Assets/Scripts/GunScript.cs
+++ Clean the spill Project/Assets/Scripts/GunScript.cs
@@ -7,12 +7,9 @@ public class GunScript : MonoBehaviour
 {
     [Header("Projectile Prefabs")]
     public GameObject projectilePrefab;
-    public GameObject ARProjectilePrefab;
     public GameObject oilProjectilePrefab;
     private Rigidbody projectile;
-    private Rigidbody ARProjectile;
     private Rigidbody oilProjectile;
-    
     [Header("Projectile Spray Settings")]
     [Tooltip("This changes how often a projectile is instantiated in sec.")]
     public float shootWaitTime = 0.1f;
@@ -22,9 +19,9 @@ public class GunScript : MonoBehaviour
     public float speed = 6;
     [Tooltip("How many projectiles can you shoot in one charge")]
     public int charge = 50; private int chargeCopy;
-    [Tooltip("How fast does the sprayer re-charge")]
-    public float chargeWaitInSeconds = 0.5f;
-
+    private float chargeWaitInSeconds = 0.5f;
+    private float deChargeWaitInSeconds = 0.5f;
+    
     [Header("")]
     public Camera fpsCamera;
     [FormerlySerializedAs("bulletSpawn")] 
@@ -41,10 +38,10 @@ public class GunScript : MonoBehaviour
     {
         projectile = projectilePrefab.GetComponentInChildren<Rigidbody>();
         oilProjectile = oilProjectilePrefab.GetComponentInChildren<Rigidbody>();
-        ARProjectile = ARProjectilePrefab.GetComponentInChildren<Rigidbody>();
 
         chargeCopy = charge;
         
+        InvokeRepeating(nameof(DeChargeGun), 0f, deChargeWaitInSeconds);
         InvokeRepeating(nameof(ChargeGun), 0f, chargeWaitInSeconds);
     }
 
@@ -80,7 +77,7 @@ public class GunScript : MonoBehaviour
         }
         else if (Input.GetKeyDown(KeyCode.Alpha2))
         {
-            Debug.Log("Ammo is Anti-Radiation");
+            Debug.Log("Ammo is Anti-Oil");
             ammoType = 1;
         }
     }
@@ -109,11 +106,10 @@ public class GunScript : MonoBehaviour
             canShoot = false;
             StartCoroutine(Wait(shootWaitTime));
         }
-        else if (ammoType == 1 && canShoot)
+        else if (ammoType == 1)
         {
-            ShootAR();
-            canShoot = false;
-            StartCoroutine(Wait(shootWaitTime));
+            Rigidbody p = Instantiate(oilProjectile, bulletSpawnTransform.transform.position, fpsCamera.transform.rotation);
+            p.velocity = -transform.forward * speed;
         }
     }
 
@@ -123,16 +119,9 @@ public class GunScript : MonoBehaviour
         p.velocity = -transform.forward * speed;
     }
 
-    private void ShootAR()
-    {
-        Rigidbody p = Instantiate(ARProjectile, bulletSpawnTransform.transform.position, fpsCamera.transform.rotation);
-        p.velocity = -transform.forward * speed;
-    }
-
     IEnumerator Wait(float waitTime)
     {
         yield return new WaitForSeconds(waitTime);
-        DeChargeGun();
         canShoot = true;
     }
 
