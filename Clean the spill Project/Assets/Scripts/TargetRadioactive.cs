7b410e720ec68ccc51c786344671c9a5fa2679bd
Mikkel Siercke <mikkelsiercke@outlook.com>
2022-04-08 13:56:04 +0200

    Radioactive prefabs

--- Clean the spill Project/Assets/Scripts/TargetRadioactive.cs
+++ Clean the spill Project/Assets/Scripts/TargetRadioactive.cs
@@ -3,18 +3,23 @@ using UnityEngine;
 
 public class TargetRadioactive : MonoBehaviour
 {
-    public float health = 2f;
-    public float damage = 0.01f;
+    public float health = 100f;
+    public GameObject character;
+    public GameObject particlePrefab;
 
-    [SerializeField] private PP_colorFilter _ppColorFilter;
+    private PP_colorFilter _ppColorFilter;
     private ParticleSystem _particles;
 
+    private float _damage;
     private bool _isCleaned;
     private bool scoreGain;
 
     private void Start()
     {
-        _particles = gameObject.GetComponent<ParticleSystem>();
+        particlePrefab = Instantiate(particlePrefab, gameObject.transform);
+        _particles = particlePrefab.GetComponent<ParticleSystem>();
+        _damage = character.GetComponentInChildren<GunScript>().damage;
+        _ppColorFilter = GameObject.FindWithTag("Player").GetComponentInChildren<PP_colorFilter>();
     }
 
     private void Update()
@@ -23,11 +28,13 @@ public class TargetRadioactive : MonoBehaviour
         {
             if (_ppColorFilter.filter)
             {
+                print(_ppColorFilter.filter);
                 if (!_particles.isPlaying)
                     _particles.Play();
             }
             else
             {
+                print(_ppColorFilter.filter);
                 if (_particles.isPlaying)
                 {
                     _particles.Clear();
@@ -43,13 +50,13 @@ public class TargetRadioactive : MonoBehaviour
         }
     }
 
-    private void OnCollisionEnter(Collision collision)
+    private void OnTriggerEnter(Collider collision)
     {
         if (!collision.gameObject.CompareTag("AntiRadiation")) return;
 
         if (health >= 0 && !_isCleaned)
         {
-            health -= damage;
+            health -= _damage;
         }
         else
         {
